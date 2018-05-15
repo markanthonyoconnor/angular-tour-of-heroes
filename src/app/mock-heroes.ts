d6187e4253c8c04b30d0dfa5a593c1128c9820ed
Mark O Connor <markanthonyoconnor@gmail.com>
2018-05-15 21:03:02 +0100

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+    {id: 11, name: 'Mr. Nice' , date: new Date},
+    {id: 12, name: 'Narco' , date: new Date},
+    {id: 13, name: 'Bombasto' , date: new Date},
+    {id: 14, name: 'Celeritas' , date: new Date},
+    {id: 15, name: 'Magmeta' , date: new Date},
+    {id: 16, name: 'Rubberman' , date: new Date},
+    {id: 17, name: 'Dynama' , date: new Date},
+    {id: 18, name: 'Dr IQ' , date: new Date},
+    {id: 19, name: 'Magma' , date: new Date},
+    {id: 20, name: 'Tornado' , date: new Date}
+
+];
